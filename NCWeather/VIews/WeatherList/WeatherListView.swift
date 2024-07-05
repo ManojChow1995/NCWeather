7308b9440ab1dc766f8aa32e69381d08a66ea484
Manoj Chowdhary <manojchowdhary1995@gmail.com>
2024-07-05 21:16:40 +1000

    Sorting options implementation

--- NCWeather/VIews/WeatherList/WeatherListView.swift
+++ NCWeather/VIews/WeatherList/WeatherListView.swift
@@ -12,6 +12,19 @@ struct WeatherListView: View {
     
     var body: some View {
         NavigationStack {
+            
+            Picker("Picker",
+                   selection: $weatherListViewModel.selectedSortOption,
+                   content:{
+                        Text("A-Z").tag(SortOptions.alphabetical)
+                        Text("Temperature").tag(SortOptions.temperature)
+                        Text("Last Updated").tag(SortOptions.lastUpdated)
+            }).padding()
+                .pickerStyle(.segmented)
+                .onChange(of: weatherListViewModel.selectedSortOption) { _, _ in
+                    weatherListViewModel.updateList()
+                }
+
             List(weatherListViewModel.weatherDataCollection) {weatherData in
                 WeatherListCellView(weatherData: weatherData)
             }
--- NCWeather/VIews/WeatherList/WeatherListViewModel.swift
+++ NCWeather/VIews/WeatherList/WeatherListViewModel.swift
@@ -7,16 +7,23 @@
 
 import Foundation
 
+enum SortOptions {
+    case alphabetical
+    case temperature
+    case lastUpdated
+}
+
 @MainActor final class WeatherListViewModel: ObservableObject {
     
     @Published var weatherDataCollection:[WeatherData] = []
+    @Published var selectedSortOption:SortOptions = .alphabetical
     
     func getWeatherData() {
         Task {
             do {
                 let unfilteredDataCollection =  try await NetworkManager.shared.fetchWeatherData()
                 weatherDataCollection = unfilteredDataCollection.filter { weatherData in
-                    if let _ = weatherData.weatherCondition {
+                    if let _ = weatherData.weatherCondition, let _ = weatherData.weatherTemp {
                         return true
                     } else {
                         return false
@@ -25,6 +32,17 @@ import Foundation
             } catch {
                 print(error)
             }
+        }
+    }
+    
+    func updateList() {
+        switch selectedSortOption {
+        case .alphabetical:
+            weatherDataCollection = weatherDataCollection.sorted{$0.name < $1.name}
+        case .temperature:
+            weatherDataCollection = weatherDataCollection.sorted{Float($0.weatherTemp ?? "") ?? 0 > Float($1.weatherTemp ?? "") ?? 0}
+        case .lastUpdated:
+            weatherDataCollection = weatherDataCollection.sorted{$0.weatherLastUpdated ?? Date() > $1.weatherLastUpdated ?? Date()}
         }
     }
 }
