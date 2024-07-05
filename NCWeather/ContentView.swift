a1e78bf9e29ad18cb65225cc1b2bfebac59bfc05
Manoj Chowdhary <manojchowdhary1995@gmail.com>
2024-07-05 11:57:46 +1000

    Initial Commit

---
+++ NCWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NCWeather
+//
+//  Created by ManChow on 5/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NCWeather/NCWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NCWeatherApp.swift
+//  NCWeather
+//
+//  Created by ManChow on 5/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct NCWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
